eae4f5a2443e95a22d5f9e9e00aeeec7f26b1479
qinleiru <qinleiru@gmail.com>
2018-04-24 21:07:53 +0800

    project

---
+++ jiuxian/public/js/jquery.toggler.js
@@ -0,0 +1,15 @@
+﻿//toggle plugin from caibaojian.com，realize the function of togggle before 1.8
+$.fn.toggler = function( fn, fn2 ) {
+    var args = arguments,guid = fn.guid || $.guid++,i=0,
+    toggler = function( event ) {
+      var lastToggle = ( $._data( this, "lastToggle" + fn.guid ) || 0 ) % i;
+      $._data( this, "lastToggle" + fn.guid, lastToggle + 1 );
+      event.preventDefault();
+      return args[ lastToggle ].apply( this, arguments ) || false;
+    };
+    toggler.guid = guid;
+    while ( i < args.length ) {
+      args[ i++ ].guid = guid;
+    }
+    return this.click( toggler );
+}
